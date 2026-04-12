8e3c92eb716624ad149c09ebf4012834a81ecda5
SALHI-1 <mohammedsalhisim@gmail.com>
2026-04-12 14:11:12 +0100

    update agent

--- src/main/java/dz/univ/hadj/AdminAgent.java
+++ src/main/java/dz/univ/hadj/AdminAgent.java
@@ -4,22 +4,31 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 import jade.lang.acl.ACLMessage;
+
 public class AdminAgent extends Agent {
     protected void setup() {
-        System.out.println("--- [Admin] Agent de supervision actif ---");
+        System.out.println("--- [Admin] Agent de supervision globale actif ---");
 
-        // Diffusion périodique des statistiques (toutes les 10 secondes)
+        // Liste des agents critiques à surveiller
+        final String[] agentsASurveiller = {"HospitalManager", "Equip", "Staff"};
+
+        // Diffusion périodique des requêtes de santé (toutes les 10 secondes)
         addBehaviour(new TickerBehaviour(this, 10000) {
             protected void onTick() {
-                System.out.println("\n--- RAPPORT SYSTEME D'INFORMATION ---");
-                System.out.println("Etat du réseau : OK");
-                System.out.println("Flux patients : Stable");
-                System.out.println("-------------------------------------\n");
-
-                ACLMessage report = new ACLMessage(ACLMessage.INFORM);
-report.addReceiver(new AID("HospitalManager", AID.ISLOCALNAME));
-report.setContent("STATUT_RESEAU_OK");
-send(report); // Cela créera une flèche toutes les 10 secondes
+                System.out.println("\n--- [Admin] SCANNER DE SANTÉ RÉSEAU ---");
+                
+                for (String nomAgent : agentsASurveiller) {
+                    // Création d'un message de type QUERY_IF (Es-tu vivant ?)
+                    ACLMessage ping = new ACLMessage(ACLMessage.QUERY_IF);
+                    ping.addReceiver(new AID(nomAgent, AID.ISLOCALNAME));
+                    ping.setContent("HEALTH_CHECK");
+                    send(ping);
+                    
+                    System.out.println(" > Vérification envoyée à : " + nomAgent);
+                }
+                
+                System.out.println("Flux patients : Stable | Statut global : OK");
+                System.out.println("-----------------------------------------\n");
             }
         });
     }
